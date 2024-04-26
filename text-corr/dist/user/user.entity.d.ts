a50961ce1a43a4214e999b5b4df5596badce2f5c
ProfCarmino <73600424+ProfCarmino@users.noreply.github.com>
2024-04-26 08:21:32 -0300

    Delete text-corr directory

--- text-corr/dist/user/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-import { Filiacao } from '../filiacao/filiacao.entity';
-export declare class User {
-    id_user: number;
-    email: string;
-    senha: string;
-    nome: string;
-    filiacao: Filiacao[];
-}
--- text-corr/src/user/user.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-
-@Module({
-  imports: [],
-  providers: [],
-  controllers: [],
-})
-export class LoginModule {}
- 
